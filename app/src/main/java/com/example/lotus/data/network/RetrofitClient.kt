2c4138289fe687b492ad193eea7a9ab859457c46
lvt-195 <thietluong2002@gmail.com>
2024-08-06 02:11:21 +0700

    update

--- app/src/main/java/com/example/lotus/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/lotus/data/network/RetrofitClient.kt
@@ -1,4 +1,15 @@
 package com.example.lotus.data.network
 
-class RetrofitClient {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://princehunganh.ddnsfree.com:7554/"
+
+    val instance: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
---
+++ app/src/main/java/com/example/lotus/utils/SharedPrefManager.kt
@@ -0,0 +1,31 @@
+package com.example.lotus.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPrefManager(context: Context) {
+
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+
+    companion object {
+        private const val SHARED_PREF_NAME = "my_shared_pref"
+        private const val KEY_IS_LOGGED_IN = "is_logged_in"
+    }
+
+    fun saveLoginState(isLoggedIn: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean(KEY_IS_LOGGED_IN, isLoggedIn)
+        editor.apply()
+    }
+
+    fun isLoggedIn(): Boolean {
+        return sharedPreferences.getBoolean(KEY_IS_LOGGED_IN, false)
+    }
+
+    fun clearLoginState() {
+        val editor = sharedPreferences.edit()
+        editor.remove(KEY_IS_LOGGED_IN)
+        editor.apply()
+    }
+}
