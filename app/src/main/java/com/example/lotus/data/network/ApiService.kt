abe338499cb2254503cb9adcfbcb853349af70c5
HoBao2k2 <baonamkka@gmail.com>
2024-08-06 10:56:05 +0700

    update api

--- app/src/main/java/com/example/lotus/data/network/ApiService.kt
+++ app/src/main/java/com/example/lotus/data/network/ApiService.kt
@@ -1,4 +1,9 @@
 package com.example.lotus.data.network
 
+import com.example.lotus.data.model.User
+import retrofit2.http.GET
+
 interface ApiService {
+    @GET("/users/profile")
+    suspend fun getUserProfile(): User
 }
