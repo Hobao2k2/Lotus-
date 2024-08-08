572bcb7eb1cb76721dcc3baa958ee402a846ce95
lvt-195 <thietluong2002@gmail.com>
2024-08-08 15:59:43 +0700

    update send token in RetrofitClient and login logout save token clear token

--- app/src/main/java/com/example/lotus/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/lotus/data/network/RetrofitClient.kt
@@ -1,24 +1,28 @@
 package com.example.lotus.data.network
 
 import android.content.Context
+import android.util.Log
 import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.example.lotus.utils.SharedPrefManager
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "http://princehunganh.ddnsfree.com:7554"
-    var token: String = ""
 
     private fun provideOkHttpClient(context : Context): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor { chain ->
+                val sharedPrefManager : SharedPrefManager = SharedPrefManager(context)
+
                 val request = chain.request()
+                val token = SharedPrefManager(context).getToken()?:""
+                Log.d("TAG", "provideOkHttpClient: $token")
                 val newRequest = request.newBuilder()
                     .addHeader(
                         "Authorization",
-                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJiYW9uYW1ra2FAZ21haWwuY29tIiwidXNlcklkIjoiNjZiMDc4ZmNiODIyOTllNGI5ZWI2MDM2IiwiaWF0IjoxNzIzMTAzMjkyLCJleHAiOjE3MjMxMDY4OTJ9.3B9mSeopVqXRAMIYWmARrkBb_Nbtu71oS0320DadUPQ"
-
+                        token
                     )
                     .build()
                 chain.proceed(newRequest)
